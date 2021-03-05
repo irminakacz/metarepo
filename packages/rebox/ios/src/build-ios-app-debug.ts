326daff32919ef667db539a22a610f2dc515d172
irmina.kaczmarek <irmina.kaczmarek@klarna.com>
2021-03-05 16:42:51 +0100

    🚧 first changes in rebox/ios

--- packages/rebox/ios/src/build-ios-app-debug.ts
+++ packages/rebox/ios/src/build-ios-app-debug.ts
@@ -26,7 +26,7 @@ export const buildIosAppDebug = async (options: TBuildIosAppDebugOptions): Promi
 
   await spawnChildProcess('pod install', {
     cwd: options.projectPath,
-    stdout: null,
+    stdout: process.stdout,
     stderr: process.stderr,
   })
 
